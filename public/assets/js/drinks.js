5acf65ffd7d7fca4446e32930d86a4890202eb0e
danlgrigg <griggdl@gmail.com>
2019-10-11 12:19:37 -0700

    Added drink block file to partials folder.

---
+++ public/assets/js/drinks.js
@@ -0,0 +1,61 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function() {
+    $(".change-empty").on("click", function(event) {
+      var id = $(this).data("id");
+      var newEmpty = $(this).data("newempty");
+  
+      var newEmptyState = {
+        empty: newEmpty
+      };
+  
+      // Send the PUT request.
+      $.ajax("/api/drinks/" + id, {
+        type: "PUT",
+        data: newEmptyState
+      }).then(
+        function() {
+          console.log("changed partytime to", empty);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".create-form").on("submit", function(event) {
+      // Make sure to preventDefault on a submit event.
+      event.preventDefault();
+  
+      var newDrink = {
+        name: $("#ca").val().trim(),
+        imbibed: $("[name=imbibed]:checked").val().trim()
+      };
+  
+      // Send the POST request.
+      $.ajax("/api/drinks", {
+        type: "POST",
+        data: newDrink
+      }).then(
+        function() {
+          console.log("created new drink");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-drink").on("click", function(event) {
+      var id = $(this).data("id");
+  
+      // Send the DELETE request.
+      $.ajax("/api/drinks/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted drink", id);
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+    });
+  });
+  
